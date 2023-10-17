61220e7a63c6ce6067be5d947a2de35328e42c0a
René Schleusner <schleusner@sitepark.com>
2023-10-17 11:32:46 +0200

    chore: remove jest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
-export default {
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['./tests/setup.js'],
-};

5c6893d8b25881e78965410e131f113b3f2374fd
René Schleusner <schleusner@sitepark.com>
2025-09-15 23:58:35 +0200

    test: don't rely on xkcd.com for tests to pass

--- examples/twig/components/context/async/async.config.js
+++ examples/twig/components/context/async/async.config.js
@@ -1,7 +1,21 @@
-import fetch from 'node-fetch';
-
-const response = fetch('https://xkcd.com/149/info.0.json').then((response) => response.json());
-
+const response = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve({
+            month: '8',
+            num: 149,
+            link: '',
+            year: '2006',
+            news: '',
+            safe_title: 'Sandwich',
+            transcript:
+                '[[ A man is sitting on a couch, talking to another man.  They are both stick figures. ]]\nFirst man:  Make me a sandwich.\nSecond man:  What?  Make it yourself.\nFirst man:  Sudo make me a sandwich.\nSecond man:  Okay',
+            alt: 'Proper User Policy apparently means Simon Says.',
+            img: 'https://imgs.xkcd.com/comics/sandwich.png',
+            title: 'Sandwich',
+            day: '28',
+        });
+    }, 100);
+});
 export default {
     context: {
         xkcd: response,
